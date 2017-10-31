5b01e23744d22c6fb3486f16ad3d09134f94faf1
Madnosam <mason_drury@hotmail.com>
2017-10-31 13:56:55 -0700

    Footer and styling for footer

--- footer.php
+++ footer.php
@@ -1,25 +1,42 @@
 			<footer class="footer" role="contentinfo" data-template="<?php echo get_page_template(); ?>" itemscope itemtype="http://schema.org/WPFooter">
 
 				<div id="inner-footer" class="wrap cf">
+					<div class="newsletter_wrapper">
+						<div class="subscribe_wrapper">
+							<span align="center">
+								Subscribe to our newsletter:
+							</span>
+							<input type="email" placeholder="Email Address">
+							<a class="subscribe_button" href="google.com">
+							</a>
+							
 
-					<nav role="navigation">
-						<?php wp_nav_menu(array(
-    					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
-    					'theme_location' => 'footer-links',             // where it's located in the theme
-    					'before' => '',                                 // before the menu
-    					'after' => '',                                  // after the menu
-    					'link_before' => '',                            // before each link
-    					'link_after' => '',                             // after each link
-    					'depth' => 0,                                   // limit the depth of the nav
-    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-						)); ?>
-					</nav>
+						</div>
+					</div>
+					<div class="sponsers_wrapper"> 
+						<a>
+							<img src="http://lorempixel.com/400/200/">
+						</a>
+						<a>
+							<img src="http://lorempixel.com/400/200/">
+						</a>
+					</div>
+					<div class="social_wrapper">
+						<p>
+							JOIN US ON
+						</p>
+						<div class="social_media_buttons">
+							<a href="www.facebook.com" style="background-image:<?php echo get_template_directory_uri(); ?>/fb.svg">
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-
+							</a>
+							<a href="www.twitter.com" style="background-image:<?php echo get_template_directory_uri(); ?>/twtr.svg" ">
+							</a>
+							<a href="www.instagram.com" style="background-image:<?php echo get_template_directory_uri(); ?>/insta.svg" ">
+							</a>
+							<a href="www.gmail.com" style="background-image:<?php echo get_template_directory_uri(); ?>/email.svg" ">
+							</a>
+						</div>
+					</div>	
 				</div>
 
 			</footer>
