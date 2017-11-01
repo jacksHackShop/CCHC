5a42374baf9aabe36da200ad68bd5b1214448c63
Madnosam <mason_drury@hotmail.com>
2017-10-31 19:36:50 -0700

    Footer 768 finished - NOTE BASE INCOMPLETE

--- footer.php
+++ footer.php
@@ -3,38 +3,44 @@
 				<div id="inner-footer" class="wrap cf">
 					<div class="newsletter_wrapper">
 						<div class="subscribe_wrapper">
-							<span align="center">
+							<span>
 								Subscribe to our newsletter:
 							</span>
 							<input type="email" placeholder="Email Address">
 							<a class="subscribe_button" href="google.com">
+								Sign Up
 							</a>
 							
 
 						</div>
 					</div>
-					<div class="sponsers_wrapper"> 
-						<a>
-							<img src="http://lorempixel.com/400/200/">
-						</a>
-						<a>
-							<img src="http://lorempixel.com/400/200/">
-						</a>
-					</div>
-					<div class="social_wrapper">
-						<p>
-							JOIN US ON
-						</p>
-						<div class="social_media_buttons">
-							<a href="www.facebook.com" style="background-image:<?php echo get_template_directory_uri(); ?>/fb.svg">
-
+					<div class="bottom_wrapper">
+						<div class="sponsers_wrapper"> 
+							<a>
+								<img src="http://lorempixel.com/400/200/">
 							</a>
-							<a href="www.twitter.com" style="background-image:<?php echo get_template_directory_uri(); ?>/twtr.svg" ">
+							<a>
+								<img src="http://lorempixel.com/400/200/">
 							</a>
-							<a href="www.instagram.com" style="background-image:<?php echo get_template_directory_uri(); ?>/insta.svg" ">
-							</a>
-							<a href="www.gmail.com" style="background-image:<?php echo get_template_directory_uri(); ?>/email.svg" ">
-							</a>
+						</div>
+						<div class="social_wrapper">
+							<p class="social_buttons_text">
+								JOIN US ON
+							</p>
+							<div class="social_media_buttons">
+								<a href="www.facebook.com">
+									<img src="<?php echo get_template_directory_uri(); ?>/library/images/fb.svg">
+								</a>
+								<a href="www.twitter.com">
+									<img src="<?php echo get_template_directory_uri(); ?>/library/images/twtr.svg">
+								</a>
+								<a href="www.instagram.com">
+									<img src="<?php echo get_template_directory_uri(); ?>/library/images/insta.svg">
+								</a>
+								<a href="www.gmail.com">
+									<img src="<?php echo get_template_directory_uri(); ?>/library/images/email.svg">
+								</a>
+							</div>
 						</div>
 					</div>	
 				</div>
