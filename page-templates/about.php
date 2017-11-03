eb4377446945feea1e1a5fe52c57c4cd497e29e4
Madnosam <mason_drury@hotmail.com>
2017-11-03 12:59:51 -0700

    Committee List Start (not finished)

--- page-templates/about.php
+++ page-templates/about.php
@@ -24,6 +24,27 @@
 								<a href="<?php echo get_field("button_1_url"); ?>" target="_blank" class="button white"><?php echo get_field("button_1_label"); ?></a>
 								<a href="<?php echo get_field("button_2_url"); ?>" target="_blank" class="button white"><?php echo get_field("button_2_label"); ?></a>
 							</div>
+							<div class="committe_list_row">
+								<?php 
+								if(have_rows("committee_list")): ?>
+									<div class="committee_list">
+									<?php
+									while(have_rows("committee_list")): the_row(); ?>
+										<div>	
+											<h2><?php the_sub_field("heading"); ?></h2>
+											<?php 
+											if(have_rows("members")): ?>
+												<ul>
+												<?php
+												while(have_rows("members")): the_row(); ?>
+													<h4> <?php the_sub_field("member_name") ?> </h4>
+												<?php endwhile; ?>
+												</ul>
+											<?php endif; ?>
+										</div>
+									<?php endwhile; ?>
+								<?php endif; ?>
+							</div>
 
 						</main>
 
