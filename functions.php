23db6f47068189a90b13b638755f379ea7da0377
gelaroo <groomiany@gmail.com>
2017-11-15 11:27:56 -0800

    Fixed tag fields for images

--- functions.php
+++ functions.php
@@ -285,7 +285,7 @@ add_action( 'init' , 'add_categories_for_attachments' );
 function add_tags_for_attachments() {
     register_taxonomy_for_object_type( 'post_tag', 'attachment' );
 }
-
+add_action( 'init' , 'add_tags_for_attachments' );
 
 
 
